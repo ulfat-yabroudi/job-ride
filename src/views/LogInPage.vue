ad84b11ded4042b1abc8284fe28e671970134d57
farahsamaha <farahsamaha121@gmail.com>
2022-05-14 16:55:08 +0300

    login page

--- src/views/LogInPage.vue
+++ src/views/LogInPage.vue
@@ -1,93 +1,183 @@
 <template>
-    <v-form
-      ref="form"
-      v-model="valid"
-      lazy-validation
-      class="mx-16"
-    >
-      <v-text-field
-        v-model="name"
-        :counter="10"
-        :rules="nameRules"
-        label="Name"
-        required
-      ></v-text-field>
-      <v-text-field
-        v-model="email"
-        :rules="emailRules"
-        label="E-mail"
-        required
-      ></v-text-field>
-      <v-select
-        v-model="select"
-        :items="items"
-        :rules="[v => !!v || 'Item is required']"
-        label="Item"
-        required
-      ></v-select>
-      <v-checkbox
-        v-model="checkbox"
-        :rules="[v => !!v || 'You must agree to continue!']"
-        label="Do you agree?"
-        required
-      ></v-checkbox>
-      <v-btn
-        :disabled="!valid"
-        color="success"
-        class="mr-4"
-        @click="validate"
-      >
-        Validate
-      </v-btn>
-      <v-btn
-        color="error"
-        class="mr-4"
-        @click="reset"
-      >
-        Reset Form
-      </v-btn>
-      <v-btn
-        color="warning"
-        @click="resetValidation"
-      >
-        Reset Validate
-      </v-btn>
-    </v-form>
+  <div class="cover">
+    <v-container class="fill-height">
+      <v-row justify="center" class="fill-height">
+        <v-col cols="12" md="4" xl="3">
+          <v-card
+            v-vue-aos="{ animationClass: 'animate__fadeIn animate__animated' }"
+            width="500"
+            height="500"
+            class="background"
+            elevation="2"
+          >
+            <v-card-title class="text-h4 justify-center teal--text">
+              LOGIN
+            </v-card-title>
+            <validation-observer ref="observer" v-slot="{ invalid }">
+              <form class="my-8 py-8" @submit.prevent="submit">
+                <validation-provider
+                  v-slot="{ errors }"
+                  name="Email"
+                  :rules="{
+                    required: true,
+                    email: 'user@gmail.com',
+                  }"
+                >
+                  <v-text-field
+                    v-model="email"
+                    class="mx-12"
+                    color="#000000"
+                    :error-messages="errors"
+                    label="Email"
+                    required
+                    @input="$v.email.$touch()"
+                    @blur="$v.email.$touch()"
+                  />
+                </validation-provider>
+
+                <validation-provider
+                  v-slot="{ errors }"
+                  name="password"
+                  rules="required"
+                >
+                  <v-text-field
+                    v-model="password"
+                    color="#000000"
+                    class="mx-12 text-dark"
+                    type="password"
+                    :rules="passwordRules"
+                    :error-messages="errors"
+                    label="Password"
+                    required
+                    @input="$v.password.$touch()"
+                    @blur="$v.password.$touch()"
+                  />
+                </validation-provider>
+                <validation-provider>
+                  <v-select
+                    class="mx-12 text-dark"
+                    v-model="select"
+                    :items="items"
+                    :error-messages="selectErrors"
+                    label="login as"
+                    required
+                    @change="$v.select.$touch()"
+                    @blur="$v.select.$touch()"
+                  ></v-select>
+                </validation-provider>
+                <v-btn
+                  class="teal accent-4 mx-16 rounded-5 white--text"
+                  type="submit"
+                  :disabled="invalid"
+                >
+                  LogIn
+                </v-btn>
+                <v-dialog v-model="dialog" width="500">
+                  <v-card>
+                    <v-card-title class="text-h5 amber lighten-2">
+                      Wrong login
+                    </v-card-title>
+
+                    <v-card-text class="text-center mt-5">
+                      Please try again
+                    </v-card-text>
+
+                    <v-divider />
+
+                    <v-card-actions>
+                      <v-spacer />
+                      <v-btn class="teal accent-4" @click="dialog = false">
+                        OK
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+
+                <v-btn
+                  class="teal accent-4 rounded-5 white--text my-9"
+                  @click="clear"
+                >
+                  clear
+                </v-btn>
+              </form>
+            </validation-observer>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
+
 <script>
+import { required, email, max } from 'vee-validate/dist/rules'
+import {
+  extend,
+  ValidationObserver,
+  ValidationProvider,
+  setInteractionMode
+} from 'vee-validate'
+
+setInteractionMode('eager')
+
+extend('required', {
+  ...required,
+  message: '{_field_} can not be empty'
+})
+
+extend('email', {
+  ...email,
+  message: 'Email must be valid'
+})
+
+extend('max', {
+  ...max,
+  message: '{_field_} may not be greater than {length} characters'
+})
 export default {
+  components: {
+    ValidationProvider,
+    ValidationObserver
+  },
   data: () => ({
-    valid: true,
-    name: '',
-    nameRules: [
-      v => !!v || 'Name is required',
-      v => (v && v.length <= 10) || 'Name must be less than 10 characters'
-    ],
     email: '',
-    emailRules: [
-      v => !!v || 'E-mail is required',
-      v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
-    ],
+    password: '',
     select: null,
-    items: [
-      'Item 1',
-      'Item 2',
-      'Item 3',
-      'Item 4'
-    ],
-    checkbox: false
+    items: ['Employee', 'Company'],
+    dialog: '',
+    passwordRules: [
+      (value) => !!value || 'Please type password.',
+      (value) => (value && value.length >= 6) || 'minimum 6 characters'
+    ]
   }),
 
   methods: {
-    validate () {
-      this.$refs.form.validate()
+    submit () {
+      this.$v.$touch()
     },
-    reset () {
-      this.$refs.form.reset()
-    },
-    resetValidation () {
-      this.$refs.form.resetValidation()
+    clear () {
+      this.password = ''
+      this.email = ''
+      this.select = null
+      this.$refs.observer.reset()
     }
   }
 }
 </script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Abel&display=swap');
+.background {
+  position: relative;
+  transform: translate(-50%, -50%);
+  top: 50%;
+  left: 50%;
+  opacity: 0.9;
+}
+.cover {
+  width: 100%;
+  height: 100vh;
+  background-size: cover;
+}
+.h4{
+   font-family: "Abel";
+}
+</style>
